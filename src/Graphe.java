ddf2590024ab9d3e03acb1d2253865e06d44bdc3
CapitainePigeon <37774136+CapitainePigeon@users.noreply.github.com>
2019-04-11 10:13:23 +0200

    composante

--- src/Graphe.java
+++ src/Graphe.java
@@ -1,5 +1,6 @@
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Graphe implements Serializable {
     public ArrayList<Mots> mes_noeuds;
@@ -28,5 +29,43 @@ public class Graphe implements Serializable {
         }
         return res;
     }
+    public ArrayList<Mots> getVoisin(Mots un_mot){
+        ArrayList<Mots> voisins=new ArrayList<>();
+        ArrayList<Aretes> aretes=getAretesMots(un_mot);
+        for (Aretes arete : aretes){
+            if(arete.noeud1.isSameas(un_mot)){
+                voisins.add(arete.noeud2);
+            }else{
+                voisins.add(arete.noeud1);
+            }
+        }
+        return  voisins;
+    }
+    public int nbComposante(){
+
+        int [] connex = new int[mes_noeuds.size()];
+        for (int i=0;i<mes_noeuds.size();i++){
+            connex[i]=0;
+        }
+        int couleur=1;
+        int i=0;
+        for( i=0;i<mes_noeuds.size();i++){
+            if(connex[i]==0){
+                rempliComposante(connex,couleur, mes_noeuds.get(i));
+                couleur=couleur+1;
+            }
+        }
+        System.out.println("NB composante :"+couleur);
+        return couleur;
+    }
+    public void rempliComposante(int [] connex,int couleur,Mots mot){
+        for (Mots  voisin :getVoisin(mot)){
+            if(connex[mes_noeuds.indexOf(voisin)]==0){
+                connex[mes_noeuds.indexOf(voisin)]=couleur;
+                rempliComposante(connex,couleur,voisin);
+            }
+        }
+    }
+
 
 }
